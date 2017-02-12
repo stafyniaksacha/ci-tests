daaccee3280267281aa4d7a1728e1844f2308b61
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-12 05:19:15 +0100

    change release tag way

--- bin/generate-tag-release.sh
+++ bin/generate-tag-release.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 set -ev
 
-printenv
-
-for i in "$@"
-do
-case $i in
-    -t=*|--gh-token=*)
-    GH_TOKEN="${i#*=}"
-    shift # past argument=value
-    ;;
-    -v=*|--version=*)
-    VERSION="${i#*=}"
-    shift # past argument=value
-    ;;
-    *)
-      # unknown option
-    ;;
-esac
-done
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 
 GITHUB_REPO="https://${GH_TOKEN:-git}@github.com/${TRAVIS_REPO_SLUG}.git"
 
@@ -26,12 +14,12 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
 git add .
-git commit -m "Release ${VERSION}"
+git commit -m "Release ${PACKAGE_VERSION}"
 
 git remote add origin-travis "$GITHUB_REPO"
-git push origin-travis "$TRAVIS_BRANCH"
+git push origin-travis HEAD:"$TRAVIS_BRANCH"
 
-git tag -d "$VERSION"
-git push origin :"$VERSION"
-git tag -a "$VERSION" -m 'Release ${VERSION}'
+git tag -d "$PACKAGE_VERSION"
+git push origin :"$PACKAGE_VERSION"
+git tag -a "$PACKAGE_VERSION" -m 'Release ${VERSION}'
 git push origin-travis --tags
