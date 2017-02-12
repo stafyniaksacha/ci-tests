bc1363fce397c9ee50360cf5c4c6d655307e2bec
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-12 05:08:42 +0100

    change release tag way

--- bin/generate-tag-release.sh
+++ bin/generate-tag-release.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
-set -v
+set -ev
 
-GITHUB_REPO=https://${GH_TOKEN:-git}@github.com/${TRAVIS_REPO_SLUG}.git
+for i in "$@"
+do
+case $i in
+    -t=*|--gh-token=*)
+    GH_TOKEN="${i#*=}"
+    shift # past argument=value
+    ;;
+    -v=*|--version=*)
+    VERSION="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+      # unknown option
+    ;;
+esac
+done
+
+GITHUB_REPO="https://${GH_TOKEN:-git}@github.com/${TRAVIS_REPO_SLUG}.git"
 
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
 git add .
-git commit -m "Release ${npm_package_version}"
+git commit -m "Release ${VERSION}"
 
-git remote add origin-travis $GITHUB_REPO
-git push origin-travis $TRAVIS_BRANCH
+git remote add origin-travis "$GITHUB_REPO"
+git push origin-travis "$TRAVIS_BRANCH"
 
-git tag -d ${npm_package_version}
-git push origin :${npm_package_version}
-git tag -a ${npm_package_version} -m 'Release ${npm_package_version}'
+git tag -d "$VERSION"
+git push origin :"$VERSION"
+git tag -a "$VERSION" -m 'Release ${VERSION}'
 git push origin-travis --tags
